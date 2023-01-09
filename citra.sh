5ee965d26ea1f8bb0f5d4eb986455bc4f12e5bac
Madboyz <101267903+Madboyz2586@users.noreply.github.com>
2023-01-09 13:49:10 -0500

    Create citra.sh

---
+++ citra.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "Installing Citra"
+mkdir emulators
+cd emulators
+echo "installing dependencies"
+sudo apt install git libsdl2-2.0-0 libsdl2-dev qtbase5-dev libqt5opengl5-dev qtmultimedia5-dev libqt5multimedia5-plugins libfdk-aac-dev ffmpeg libavcodec58 libavcodec58-dev build-essential libc++-dev cmake clang
+git clone --recursive https://github.com/citra-emu/citra
+cd citra
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release -DENABLE_FFMPEG=ON ..
+cmake --build . -- -j"$(nproc)"
+echo "Test Emulator if it dosen't open or work cancel the script"
+cd bin
+./citra-qt
+echo "Continuing"
+cd ..
+sudo make install
+echo "Installed Citra"
