dfbca2d67d36598c9b64ad4b88b09a9980a18917
Madboyz <101267903+Madboyz2586@users.noreply.github.com>
2023-01-10 08:17:27 -0500

    Update citra.sh

--- citra.sh
+++ citra.sh
@@ -3,7 +3,7 @@ echo "Installing Citra"
 mkdir emulators
 cd emulators
 echo "installing dependencies"
-sudo apt install git libsdl2-2.0-0 libsdl2-dev qtbase5-dev libqt5opengl5-dev qtmultimedia5-dev libqt5multimedia5-plugins libfdk-aac-dev ffmpeg libavcodec58 libavcodec58-dev build-essential libc++-dev cmake clang
+sudo apt install git libsdl2-2.0-0 libsdl2-dev qtbase5-dev libqt5opengl5-dev qtmultimedia5-dev libqt5multimedia5-plugins libfdk-aac-dev ffmpeg libavcodec58 build-essential libc++-dev cmake clang
 git clone --recursive https://github.com/citra-emu/citra
 cd citra
 mkdir build
