fed95340f5960e897580c7947f573a91ed5c789d
Madboyz <101267903+Madboyz2586@users.noreply.github.com>
2023-01-10 10:31:52 -0500

    Create flathub-emulators.sh

---
+++ flathub-emulators.sh
@@ -0,0 +1,13 @@
+sudo apt install flathub -y
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak install flathub org.scummvm.ScummVM
+flatpak install flathub org.flycast.Flycast
+flatpak install flathub org.duckstation.DuckStation
+flatpak install flathub org.libretro.RetroArch
+flatpak install flathub org.srb2.SRB2
+flatpak install flathub org.srb2.SRB2Kart
+flatpak install flathub org.mamedev.MAME
+flatpak install flathub com.dosbox_x.DOSBox-X
+flatpak install flathub io.github.simple64.simple64
+flatpak install flathub io.openrct2.OpenRCT2
+flatpak install flathub net.kuribo64.melonDS
